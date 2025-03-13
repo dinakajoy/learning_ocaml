79d9b24f1b30adb16415da93d6626a0746747a16
dinakajoy <dinakajoy@gmail.com>
2025-03-13 22:47:08 +0100

    working on toda app

--- todo_list_app/bin/main.ml
+++ todo_list_app/bin/main.ml
@@ -1,19 +1,62 @@
+open Yojson.Basic
+open Yojson.Basic.Util
 
+let json_file = "todos.json"
+
+(* Read JSON file *)
 let read_json filename =
-  let json = Yojson.Basic.from_file filename in
+  try Yojson.Basic.from_file filename
+  with _ -> `List [] 
+
+let write_json filename json =
+  let oc = open_out filename in
+  Printf.fprintf oc "%s\n" (Yojson.Basic.pretty_to_string json);
+  close_out oc
+
+let print_json filename =
+  let json = read_json filename in
   print_endline (Yojson.Basic.pretty_to_string json)
 
-(* let add_todo () = 
-  let todos = read_json "todos.json" in
-  todos
+let add_task json title description =
+  match json with
+  | `List lst -> `List (lst @ [`Assoc [("title", `String title); ("description", `String description); ("completed", `Boolean false)]])
+  | _ -> failwith "Invalid JSON format"
 
-let remove_todo () = "Removed"
+let add_todo ?(description="") title = 
+  let json = read_json json_file in
+  let updated_json = add_task json title description in
+  write_json json_file updated_json
 
-let mark_completed () = "Completed"
 
-let list_todos () = "All Todos" *)
+  (* let todos = read_json "todos.json" in
+  todos *)
 
+(* let remove_todo () = "Removed"
 
-let () = 
-  let todos = read_json "todos.json" in
-  todos
+let mark_completed () = "Completed" *)
+
+let view_todos () = print_json json_file
+
+let () =
+  print_endline "To-Do List CLI App";
+  print_endline "To add new task, enter 1: ";
+  print_endline "To view all tasks, enter 2: ";
+  print_endline "to view completed tasks, enter 3: ";
+  print_endline "To view pending tasks, enter 4: ";
+  print_endline "To mark a task as completed, enter 5: ";
+  print_endline "To delete a task, enter 6: ";
+  print_endline "To exit, enter 0: ";
+
+  print_string "Enter your choice (1, 2, 3, 4, 5, 6, or 0): ";
+
+  let choice = read_line () in
+    match choice with 
+    (* | "1" -> add_todo () *)
+    | "1" -> view_todos ()
+    | "2" -> view_todos ()
+    | "3" -> view_todos ()
+    | "4" -> view_todos ()
+    | "5" -> view_todos ()
+    | "6" -> view_todos ()
+    | "0" -> view_todos ()
+    | _ -> ()
